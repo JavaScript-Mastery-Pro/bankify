e34e21c204d24da7058286c05cfbf8c2ea40fcca
developer-anil-magar <anilmagar.developer@gmail.com>
2024-04-12 01:12:57 +0545

    sign in and sign up layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+const RootLayoutAuth = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main className="flex h-screen w-full justify-between font-inter">
+      {children}
+      <div className="flex size-full items-center justify-end bg-sky-1 max-lg:hidden">
+        <div className="rounded-xl border-y-[6px] border-l-[6px] border-gray-900">
+          <Image
+            src="/icons/auth-image.svg"
+            alt="Auth Image"
+            width={500}
+            height={500}
+            className="rounded-l-xl object-contain"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayoutAuth;
