1270a6207f79450cbe2cd870ba4b4c8be9f46b1b
nikkyeya-jsm <nikky@jsmastery.pro>
2024-04-19 15:27:04 +0800

    account form with pliad

--- components/shared/AuthForm.tsx
+++ components/shared/AuthForm.tsx
@@ -1,19 +1,14 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ID } from "appwrite";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 import { PlaidLink } from "@/components/shared/PlaidLink";
-import { useUserContext } from "@/context/AuthContext";
-import { createLinkToken } from "@/lib/actions/user.actions";
-import { account } from "@/lib/appwrite/config";
 import { loginUser } from "@/lib/services/index";
 
 import { Button } from "../ui/button";
@@ -28,18 +23,20 @@ import {
 import { Input } from "../ui/input";
 
 interface AuthFormProps {
-  type: "sign-in" | "sign-up" | "connect";
-  id?: string;
+  type: "sign-in" | "sign-up";
 }
 
-const AuthForm = ({ type, id }: AuthFormProps) => {
-  const router = useRouter();
+const AuthForm = ({ type }: AuthFormProps) => {
+  // const router = useRouter();
+  // const { setIsAuthenticated, isAuthenticated } = useUserContext();
+  // if (isAuthenticated) {
+  //   router.push("/");
+  // }
 
-  const { setIsAuthenticated } = useUserContext();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [accountId, setAccountId] = useState("");
   const [linkToken, setLinkToken] = useState(null);
+  // console.log({ linkToken });
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const formSchema = z.object({
     name:
@@ -47,6 +44,10 @@ const AuthForm = ({ type, id }: AuthFormProps) => {
         ? z.string().optional()
         : z.string().min(3, "name cannot be empty"),
     email: z.string().email(),
+    ssn:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(3, "ssn cannot be empty"),
     password: z.string().min(8, "password must be 8 character"),
   });
 
@@ -55,62 +56,142 @@ const AuthForm = ({ type, id }: AuthFormProps) => {
     defaultValues: {
       name: "",
       email: "",
+      ssn: "",
       password: "",
     },
   });
 
   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-
     try {
-      if (type === "sign-up") {
-        const userData = {
-          email: data.email,
-          name: data.name,
-          password: data.password,
-        };
+      // ========================================
+      // SIGN-UP TO APPWRITE & CREATE PLAID PUBLIC TOKEN
+      // if (type === "sign-in") {
+      //   const session = await loginUser({
+      //     email: data.email,
+      //     password: data.password,
+      //   });
 
-        // Create appwrite account
-        const newAppwriteAccount = await account.create(
-          ID.unique(),
-          userData.email,
-          userData.password,
-          userData.name
-        );
+      //   if (session) {
+      //     await fetch(
+      //       `${process.env.NEXT_PUBLIC_SITE_URL}/api/plaid/create_link_token`,
+      //       {
+      //         method: "POST",
+      //         headers: {
+      //           "Content-Type": "application/json",
+      //         },
+      //         body: JSON.stringify({ userId: session.userId }),
+      //       }
+      //     )
+      //       .then((response) => response.json())
+      //       .then((data) => {
+      //         const { publicToken } = data;
+      //         if (publicToken) {
+      //           setLinkToken(publicToken);
+      //         }
+      //       });
+      //   }
+      // }
 
-        if (newAppwriteAccount) {
-          const accountId = newAppwriteAccount?.$id as string;
-
-          router.push(`/connect/${accountId}`);
-          setAccountId(accountId);
+      // ========================================
+      // SANDBOX DATA
+      await fetch(
+        `${process.env.NEXT_PUBLIC_SITE_URL}/api/plaid/sandbox_link_tokens_create`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      }
+      )
+        .then((response) => response.json())
+        .then(async (data) => {
+          const { accessToken, accountId } = data;
+          console.log({ accessToken });
+          console.log({ accountId });
 
-      if (type === "sign-in") {
-        const session = await loginUser({
-          email: data.email,
-          password: data.password,
+          if (accessToken) {
+            // ========================================
+            // TRANSFER
+            // await fetch(
+            //   `${process.env.NEXT_PUBLIC_SITE_URL}/api/plaid/transfer/create`,
+            //   {
+            //     method: "POST",
+            //     headers: {
+            //       "Content-Type": "application/json",
+            //     },
+            //     body: JSON.stringify({
+            //       accountId,
+            //       accessToken,
+            //     }),
+            //   }
+            // )
+            //   .then((response) => response.json())
+            //   .then((data) => {
+            //     const { transfer } = data;
+            //     if (transfer) {
+            //       console.log(transfer);
+            //     }
+            //   });
+
+            // ========================================
+            // TRANSACTIONS
+            await fetch(
+              `${process.env.NEXT_PUBLIC_SITE_URL}/api/plaid/transactions`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  accountId,
+                  accessToken,
+                  startDate: "2018-01-01",
+                  endDate: "2018-02-01",
+                }),
+              }
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                const { added } = data;
+                if (added) {
+                  console.log(added);
+                }
+              });
+          }
         });
 
-        if (session) {
-          setIsAuthenticated(true);
-          router.push("/");
-        }
-      }
+      // ========================================
+
+      // if (type === "sign-up" && data.name && data.ssn) {
+      //   const userData = {
+      //     email: data.email,
+      //     name: data.name,
+      //     password: data.password,
+      //     ssn: data.ssn,
+      //   };
+
+      //   const onboardingLink = await signUpUser(userData);
+
+      //   if (onboardingLink) {
+      //     window.location.href = onboardingLink.url;
+      //   }
+      // }
+
+      // if (type === "sign-in") {
+      //   const session = await loginUser({
+      //     email: data.email,
+      //     password: data.password,
+      //   });
+
+      //   if (session) {
+      //     setIsAuthenticated(true);
+      //     router.push("/");
+      //   }
+      // }
       setIsLoading(false);
     } catch (error) {
       console.log(error);
-    }
-  };
-
-  const generateToken = async () => {
-    if (id) {
-      const token = await createLinkToken(id);
-
-      if (token) {
-        setLinkToken(token);
-        console.log({ linkToken });
-      }
     }
   };
 
@@ -124,69 +205,81 @@ const AuthForm = ({ type, id }: AuthFormProps) => {
           </h1>
         </Link>
         <div className="flex flex-col gap-1 md:gap-3">
-          <h1 className="text-24 lg:text-36 font-semibold capitalize text-gray-900">
-            {type}
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {type === "sign-in" ? "Sign in" : "Sign up"}
           </h1>
           <p className="text-16 font-normal text-gray-600">
-            {type === "connect"
-              ? "Generate a token to add bank"
-              : "Please enter your details."}
+            Please enter your details.
           </p>
         </div>
       </header>
-
-      {type === "connect" ? (
-        <Button
-          type="button"
-          className="text-16 rounded-lg border border-bankGradient bg-bank-gradient font-semibold text-white shadow-form"
-          onClick={generateToken}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex flex-col gap-5 md:gap-8"
         >
-          Generate Token
-        </Button>
-      ) : (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="flex flex-col gap-5 md:gap-8"
-          >
-            {type === "sign-up" && (
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <div className="flex flex-col gap-1.5">
-                      <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
-                        Name
-                      </FormLabel>
-                      <div className="flex w-full flex-col">
-                        <FormControl>
-                          <Input
-                            placeholder="Enter account holder name"
-                            className="input-class"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage className="text-12 text-red-500" />
-                      </div>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex flex-col gap-1.5">
+                    <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                      Name
+                    </FormLabel>
+                    <div className="flex w-full flex-col">
+                      <FormControl>
+                        <Input
+                          placeholder="Enter account holder name"
+                          className="input-class"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="text-12 text-red-500" />
                     </div>
-                  </FormItem>
-                )}
-              />
+                  </div>
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-col gap-1.5">
+                  <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                    Email
+                  </FormLabel>
+                  <div className="flex w-full flex-col">
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email"
+                        className="input-class"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-12 text-red-500" />
+                  </div>
+                </div>
+              </FormItem>
             )}
+          />
+          {type === "sign-up" && (
             <FormField
               control={form.control}
-              name="email"
+              name="ssn"
               render={({ field }) => (
                 <FormItem>
                   <div className="flex flex-col gap-1.5">
                     <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
-                      Email
+                      Social Security Number
                     </FormLabel>
                     <div className="flex w-full flex-col">
                       <FormControl>
                         <Input
-                          placeholder="Enter your email"
+                          placeholder="000000000"
                           className="input-class"
                           {...field}
                         />
@@ -197,53 +290,53 @@ const AuthForm = ({ type, id }: AuthFormProps) => {
                 </FormItem>
               )}
             />
+          )}
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex flex-col gap-1.5">
-                    <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
-                      Password
-                    </FormLabel>
-                    <div className="flex w-full flex-col">
-                      <FormControl>
-                        <Input
-                          placeholder="••••••••"
-                          className="input-class"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage className="text-12 text-red-500" />
-                    </div>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-col gap-1.5">
+                  <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                    Password
+                  </FormLabel>
+                  <div className="flex w-full flex-col">
+                    <FormControl>
+                      <Input
+                        placeholder="••••••••"
+                        className="input-class"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-12 text-red-500" />
                   </div>
-                </FormItem>
+                </div>
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-4">
+            <Button
+              type="submit"
+              className="text-16 rounded-lg border border-bankGradient bg-bank-gradient font-semibold text-white shadow-form"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 size={20} className="animate-spin" /> &nbsp;
+                  Loading...
+                </>
+              ) : type === "sign-in" ? (
+                "Sign in"
+              ) : (
+                "Sign up"
               )}
-            />
-            <div className="flex flex-col gap-4">
-              <Button
-                type="submit"
-                className="text-16 rounded-lg border border-bankGradient bg-bank-gradient font-semibold text-white shadow-form"
-              >
-                {isLoading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp;
-                    Loading...
-                  </>
-                ) : type === "sign-in" ? (
-                  "Sign in"
-                ) : (
-                  "Sign up"
-                )}
-              </Button>
-            </div>
-          </form>
-        </Form>
-      )}
+            </Button>
+          </div>
+        </form>
+      </Form>
 
       <footer className="flex justify-center gap-1">
-        {accountId !== "" && <PlaidLink linkToken={linkToken} />}
+        {linkToken != null ? <PlaidLink linkToken={linkToken} /> : <></>}
         <p className="text-14 font-normal text-gray-600">
           {type === "sign-in"
             ? "Don't have an account?"
