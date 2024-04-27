9383a34a884334ba4b7b466aa8ed5b30aec0a180
sujatagunale <sujata@jsmastery.pro>
2024-04-27 20:51:41 +0530

    fix: footer and counter

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -3,9 +3,11 @@
 import CountUp from "react-countup";
 
 function AnimatedCounter({ amount }: { amount: string }) {
+  const numericAmount = Number(amount.replace(/[^0-9.-]+/g, ""));
+
   return (
     <div className="w-full">
-      <CountUp decimals={2} decimal="," prefix="$" end={Number(amount)} />
+      <CountUp decimals={2} end={Number(numericAmount)} />
     </div>
   );
 }
